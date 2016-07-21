b05ce0758ccd50f8f0a6cacc6acfb52190ecc355
kannankumar <kannanpkumar@gmail.com>
2016-07-22 02:30:06 +0530

    Added the chat logic and handlers to controller

--- C.controller.js
+++ C.controller.js
@@ -6,22 +6,152 @@ sap.ui.define([
 	"use strict";
 
 	var CController = Controller.extend("kpk.kandroid.C", {
+		onInit: function () {
+			var oThis = this;
+			oThis.accessToken = "0a863377b34143728d5b3323b14e8fc7";
+			oThis.baseUrl = "https://api.api.ai/v1/";
+			oThis.bSayHiDone = false;
+			oThis.recognition;
+		},
+		onAfterRendering: function () {
 
-		onListItemPress: function (oEvent) {
-			MessageToast.show('Item Pressed: ' + oEvent.getSource().getTitle());
+		},
+		handleMsgSendBtnPress: function () {
+			this.send();
+		},
+		handleMicBtnPress: function () {
+			this.switchRecognition();
+		},
+		startRecognition: function () {
+			var oThis = this;
+			var userMsgListItem;
+			oThis.recognition = new webkitSpeechRecognition();
+			oThis.recognition.onstart = function (event) {
+
+				userMsgListItem = new sap.m.NotificationListItem({
+					description: "Listening ...",
+					showCloseButton: false,
+					//dateTime: new Date(),
+					priority: "Low",
+					authorName: "Me",
+					authorPicture: "user-icon.png"
+				})
+				oThis.oView.byId('chatMsgGrid').addContent(userMsgListItem);
+
+				oThis.updateRec("start");
+			};
+			oThis.recognition.onresult = function (event) {
+				var text = "";
+				for (var i = event.resultIndex; i < event.results.length; ++i) {
+					text += event.results[i][0].transcript;
+				}
+
+				oThis.oView.byId('chatMsgGrid').removeContent(userMsgListItem);
+
+				oThis.setInput(text);
+				oThis.stopRecognition();
+			};
+			oThis.recognition.onend = function () {
+				oThis.stopRecognition();
+			};
+			oThis.recognition.lang = "en-US";
+			oThis.recognition.start();
 		},
 
-		onRejectPress: function () {
-			MessageToast.show('Reject Button Pressed');
+		stopRecognition: function () {
+			var oThis = this;
+			if (oThis.recognition) {
+				oThis.recognition.stop();
+				oThis.recognition = null;
+			}
+			this.updateRec("stop");
 		},
 
-		onAcceptPress: function () {
-			MessageToast.show('Accept Button Pressed');
+		switchRecognition: function () {
+			var oThis = this;
+			if (oThis.recognition) {
+				this.stopRecognition();
+			} else {
+				this.startRecognition();
+			}
 		},
 
-		onItemClose: function (oEvent) {
-			MessageToast.show('Item Closed: ' + oEvent.getSource().getTitle());
+		setInput: function (text) {
+			this.oView.byId("chatInput").setValue(text);
+			//Add to chat list on right
+			this.send();
+		},
+
+		updateRec: function (sEvent) {
+			switch (sEvent) {
+			case "start":
+			case "stop":
+			}
+		},
+
+		send: function () {
+			var oThis = this;
+			if (!oThis.bSayHiDone) {
+				oThis.bSayHiDone = true;
+				this.oView.byId("chatInput").setPlaceholder('Type a message');
+			}
+			var text = this.oView.byId("chatInput").getValue();
+			this.setUserInput(text); //Adding the text to chat box
+			this.oView.byId("chatInput").setValue(""); //Clearing Input box
+
+			$.ajax({
+				type: "POST",
+				url: oThis.baseUrl + "query/",
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				headers: {
+					"Authorization": "Bearer " + oThis.accessToken
+				},
+				data: JSON.stringify({
+					q: text,
+					lang: "en"
+				}),
+
+				success: function (data) {
+					var msg = new SpeechSynthesisUtterance(data.result.speech);
+					window.speechSynthesis.speak(msg);
+
+					oThis.setResponse(data.result.speech);
+				},
+				error: function () {
+					oThis.setResponse("Internal Server Error");
+				}
+			});
+			//setResponse("Loading...");
+		},
+
+		setUserInput: function (msg) {
+			var userMsgListItem = new sap.m.NotificationListItem({
+				description: msg,
+				showCloseButton: false,
+				//dateTime: new Date(),
+				priority: "Low",
+				authorName: "Me",
+				authorPicture: "user-icon.png"
+			})
+			this.oView.byId('chatMsgGrid').addContent(userMsgListItem);
+			console.log(msg);
+		},
+
+		setResponse: function (msg) {
+			var kandroidMsgListItem = new sap.m.NotificationListItem({
+				description: msg,
+				showCloseButton: false,
+				//dateTime: new Date(),
+				priority: "None",
+				authorName: "Kandroid",
+				authorPicture: "kandroid-logo.png"
+			})
+			this.oView.byId('chatMsgGrid').addContent(kandroidMsgListItem);
+			console.log(msg);
+
 		}
+
 	});
 
 	return CController;
